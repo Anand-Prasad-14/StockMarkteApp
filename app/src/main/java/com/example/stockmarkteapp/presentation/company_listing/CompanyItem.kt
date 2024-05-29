98e93b2eb9ca51d55ec77c6e5318cf1e880e3c72
Anand-Prasad-14 <adrock4321anandgp@gmail.com>
2024-05-29 13:18:01 +0530

    CompanyListViewModel, CompanyListScreen, DI Setup

--- app/src/main/java/com/example/stockmarkteapp/presentation/company_listing/CompanyItem.kt
+++ app/src/main/java/com/example/stockmarkteapp/presentation/company_listing/CompanyItem.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -37,7 +38,7 @@ fun CompanyItem(
                     text = company.name,
                     fontWeight = FontWeight.SemiBold,
                     fontSize = 16.sp,
-                    color = MaterialTheme.colorScheme.background,
+                    color = MaterialTheme.colorScheme.onBackground,
                     overflow = TextOverflow.Ellipsis,
                     maxLines = 1,
                     modifier = Modifier.weight(1f)
@@ -46,14 +47,14 @@ fun CompanyItem(
                 Text(
                     text = company.exchange,
                     fontWeight = FontWeight.Light,
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.onBackground
                 )
 
             }
             Spacer(modifier = Modifier.height(8.dp))
             Text(text = "(${company.symbol})",
                 fontStyle = FontStyle.Italic,
-                color = MaterialTheme.colorScheme.background)
+                color = MaterialTheme.colorScheme.onBackground)
         }
     }
 }
--- app/src/main/java/com/example/stockmarkteapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/stockmarkteapp/ui/theme/Theme.kt
@@ -10,21 +10,24 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+primary = Color.Green,
+    background = DarkBlue,
+    onPrimary = Color.DarkGray,
+    onBackground = TextWhite
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color.Green,
+    background = TextWhite,
+    onPrimary = Color.DarkGray,
+    onBackground = DarkBlue
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
